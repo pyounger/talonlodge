9868e936266b5055b1f829e3f1f24c365a322fcc
pyounger <phil@talonlodge.com>
2018-02-22 02:46:12 -0500

    no message

---
+++ profiles/js/bootstrap-timepicker-custom.js
@@ -0,0 +1,32 @@
+$(function () {
+
+
+    // Time Pickers
+    $('#timepicker1').timepicker();
+
+    $('#timepicker2').timepicker({
+        minuteStep: 1,
+        showSeconds: true,
+        showMeridian: true,
+        defaultTime: false
+    });
+    $('#timepicker3').timepicker({
+        minuteStep: 1,
+        showSeconds: true,
+        showMeridian: true,
+        defaultTime: false
+    });
+    $('#timepicker4').timepicker({
+        minuteStep: 1,
+        showSeconds: true,
+        showMeridian: true,
+        defaultTime: false
+    });
+    $('.mytimepicker').timepicker({
+        minuteStep: 1,
+        showSeconds: true,
+        showMeridian: true,
+        defaultTime: false
+    });
+    
+});
